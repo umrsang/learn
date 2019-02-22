e0daf785f428c233e16e21c6fd70cca8f7041c47
zenglingbiao <zenglingbiao@aobi.com>
2019-02-22 10:46:41 +0800

    up

---
+++ fairygui/d_1/out/Package1/Package1Binder.ts
@@ -0,0 +1,10 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module Package1 {
+
+	export class Package1Binder{
+		public static bindAll():void {
+
+		}
+	}
+}
