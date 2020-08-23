8dfc5629fbf789aa5418961049e07b695aace9df
zenlingbiao <458699130@qq.com>
2020-08-23 18:30:35 +0800

    整理

--- gulp/demo-1/src/js/index.js
+++
@@ -1,8 +0,0 @@
-let oo1 = {
-  text: 'hello',
-  log(){
-    console.log(this.text)
-  }
-};
-
-oo1.log();
--- image/app.js
+++
@@ -1,5 +0,0 @@
-var images = require("images");
-
-images("./img/001.png").save("./output/001.png", {               //Save the image to a file, with the quality of 50
-  quality : 50                    //保存图片到文件,图片质量为50
-});     
